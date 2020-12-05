4b95a2adeac591d2d65a9dc600e0706759da77c5
Michael <stall84@gmail.com>
2020-12-05 16:39:13 -0500

    changed for attribute to htmlFor in testcomp form

--- src/components/TestComp/TestComp.jsx
+++ src/components/TestComp/TestComp.jsx
@@ -8,7 +8,7 @@ export const TestComp = () => (
     <div className={styles.label}>
         <h3>INPUT FORM</h3>
         <form>   
-            <label  for="username">Username: </label>
+            <label  htmlFor="username">Username: </label>
             <input type="text" id="username"></input>
         </form>
     </div>
