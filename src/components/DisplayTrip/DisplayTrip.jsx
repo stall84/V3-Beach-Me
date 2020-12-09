8d4426f2bd802017367974ae4fda589925b05fab
Michael <stall84@gmail.com>
2020-12-09 02:15:42 -0500

    ADDED: Several Material-UI Card features for displaying each beach/trip and later weather.

--- src/components/DisplayTrip/DisplayTrip.jsx
+++ src/components/DisplayTrip/DisplayTrip.jsx
@@ -1,53 +1,83 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+import clsx from 'clsx';
 
 import { timeConverter } from '../../utilities/utilities';
 import styles from './displayTrip.module.css';
 
+import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
+import IconButton from '@material-ui/core/IconButton';
+import Collapse from '@material-ui/core/Collapse';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 
-const dummyArr = [
-    {
-        beachName: 'Destin',
-        distance: '4hr 30min'
+const useStyles = makeStyles((theme) => ({
+    // root: {
+    //   maxWidth: 345,
+    // },
+    // media: {
+    //   height: 0,
+    //   paddingTop: '56.25%', // 16:9
+    // },
+    expand: {
+      transform: 'rotate(0deg)',
+      marginLeft: 'auto',
+      transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+      }),
     },
-    {
-        beachName: 'Pensacola',
-        distance: '3hr 45min'
+    expandOpen: {
+      transform: 'rotate(180deg)',
     },
-    {
-        beachName: 'Sarasota',
-        distance: '5hr 45min'
-    },
-    {
-        beachName: 'Hilton Head Island',
-        distance: '2hr 5min'
-    },
-    {
-        beachName: 'Miami Beach',
-        distance: '6hr 15min'
-    }
-];
+    // avatar: {
+    //   backgroundColor: red[500],
+    // },
+  }));
+
 
     function TripCards(props) {
         const latitude = useSelector((state) => state.latitude);
         const longitude = useSelector((state) => state.longitude);
         const beachFive = useSelector((state) => state.beaches);
+
+        const classes = useStyles();
+        const [expanded, setExpanded] = React.useState(false);
+
+        const handleExpandClick = () => {
+            setExpanded(!expanded);
+        };
+
         return (
             <Card className={styles.base_style}>
-            <CardHeader title={props.trip.name} />
+            <CardHeader  title={props.trip.name} />
 
             <CardContent>
             <a className={styles.anchor_tag} target='_blank' rel='noopener noreferrer' href={`https://www.google.com/maps/dir/?api=1&origin=${latitude},${longitude}&destination=${props.trip.name}&travelmode=driving`}>
                 {timeConverter(props.trip.dur)}
             </a>
             </CardContent>
-
+            <CardActions disableSpacing>
+            <IconButton
+                className={clsx(classes.expand, {
+                    [classes.expandOpen]: expanded,
+                })}
+                onClick={handleExpandClick}
+                aria-expanded={expanded}
+                aria-label="show more"
+            >
+            <ExpandMoreIcon />
+            </IconButton>
+            </CardActions>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+            <CardContent>
+                <p>TEST AREA FOR WEATHER</p>
+            </CardContent>
+            </Collapse>
             </Card>
         )
     }
